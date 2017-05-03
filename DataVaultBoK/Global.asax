a00fbc8021a4aaec6a0005d2dc83bb3331f77b29
david <david@MALLY>
2017-05-03 19:21:10 +1000

    Add project files.

---
+++ DataVaultBoK/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataVaultBoK.MvcApplication" Language="C#" %>
---
+++ DataVaultBoK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataVaultBoK.Startup))]
+namespace DataVaultBoK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
